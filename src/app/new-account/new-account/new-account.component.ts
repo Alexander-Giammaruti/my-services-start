36dde8b4389d8f2216874e5111f25e1b7ba94abb
Alexander Giammaruti <44341602+Alexander-Giammaruti@users.noreply.github.com>
2020-12-05 18:27:09 -0600

    Added an account service to hold account data

--- src/app/new-account/new-account/new-account.component.ts
+++ src/app/new-account/new-account/new-account.component.ts
@@ -1,22 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { AccountService } from 'src/app/account.service';
 import { LoggingService } from '../../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService,/* AccountService */] // this created a unique instance of the account array. This behavior is not desired for this application
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
   
-  constructor(private loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }

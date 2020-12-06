4c645392ce76131c0708b419beda01c1d7e70869
Alexander Giammaruti <44341602+Alexander-Giammaruti@users.noreply.github.com>
2020-12-05 18:36:08 -0600

    centralized all services by injecting logging into accounts

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from '@angular/core';
+
+@Injectable() // not necessarily needed, but in future it might be needed. Nothing is changed by using this decorator
 export class LoggingService {
     logStatusChange(status: string) {
         console.log('A server status has changed to ' + status);
--- src/app/new-account/new-account/new-account.component.ts
+++ src/app/new-account/new-account/new-account.component.ts
@@ -6,7 +6,7 @@ import { LoggingService } from '../../logging.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService,/* AccountService */] // this created a unique instance of the account array. This behavior is not desired for this application
+  //providers: [LoggingService,/* AccountService */] // this created a unique instance of the account array. This behavior is not desired for this application
 })
 export class NewAccountComponent {
   
@@ -16,6 +16,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
